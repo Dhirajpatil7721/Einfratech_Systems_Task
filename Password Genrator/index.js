a09f6c3283af43cec7783cb665108c94ff7d23f6
Dhiraj Patil <151619876+Dhirajpatil7721@users.noreply.github.com>
2025-01-31 00:50:47 +0530

    Add files via upload

---
+++ Password Genrator/index.js
@@ -0,0 +1,48 @@
+let genrate = document.getElementById("genrate")
+let display = document.getElementById("genrateinp")
+let copy = document.getElementById("copy")
+let len8 = document.getElementById("len8")
+let len10 = document.getElementById("len10")
+let upper = document.getElementById("upper")
+let lower = document.getElementById("lower")
+let char = document.getElementById("char")
+
+
+genrate.addEventListener('click', () => {
+    let len = 8;
+
+    if (len10.checked) len = 10;
+
+    let charat = '';
+
+    if (upper.checked) charat += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    if (lower.checked) charat += 'abcdefghijklmnopqrstuvwxyz'
+    if (char.checked) charat += '!@#$%^&*()_-+=<>?'
+
+    if (charat === '') {
+        alert("Please select at least one character type")
+        return;
+    }
+
+    let pass = '';
+
+    for (i = 0; i < len; i++) {
+        pass += charat.charAt(Math.floor(Math.random() * charat.length));
+    }
+    display.value = pass
+})
+
+const copytxt = () => {
+    if (display.value === '') {
+        alert("No Text")
+    }
+    else {
+
+        display.select();
+        display.setSelectionRange(0, 99999);
+
+        document.execCommand("copy");
+
+        alert("Copied: " + display.value);
+    }
+}
